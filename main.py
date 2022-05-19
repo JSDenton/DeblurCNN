122d70cf36e408585b78c2e67c46fff43243c113
JSDenton <39029962+JSDenton@users.noreply.github.com>
2022-05-19 14:25:43 +0200

    reading and blurring images

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
-from IPython.display import HTML
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -23,21 +23,43 @@ except ImportError:
 
 #SETTINGS
 
-settings = {} #dictionary with a settings (that's a global variable)
+sets = {} #dictionary with a settings (that's a global variable)
 img_size = 0
 
 def read_settings(filename): #filename with its extension
-    global settings
+    global sets
     with open(filename, 'r') as file:
-        settings = load(file, Loader = Loader) #read the settings yaml file
+        sets = load(file, Loader = Loader) #read the settings yaml file
         #print(type(settings))
                 
+def get_images():
+    path = os.listdir(sets['dataset_path'])
+    images = []
+    if len(path)<=0:
+        print("WRONG PATH")
+        return
+    for i in path:
+        imgs_in_folder = os.listdir(f"{sets['dataset_path']}/{i}")
+        img1 = cv2.imread(imgs_in_folder[0], cv2.IMREAD_COLOR)
+        img2 = cv2.imread(imgs_in_folder[len(imgs_in_folder)-2], cv2.IMREAD_COLOR)
+        images.append(img1)
+        images.append(img2)
+    print("The images have been read ...")
+    return images
 
+def gauss_blur(images):
+    os.makedirs('../blurred', exist_ok = True)
+    for i in range(len(images)):
+        images[i] = cv2.GaussianBlur(images[i], (31, 31), 0)
+        cv2.imwrite(f"../blurred/{images[i]}", blur)
+
+    return images
 
 def __main__():
     if __name__!='__main__':
         return
     read_settings("settings.yaml")
+
     #print(settings)
     start_time = time.time()
     dataset = dset.ImageFolder(root = settings.get('dataset_path'),
